fe15428f3ae2ea91f90b05fa23d324a6d98b4bfc
Abdullah Mohamed <101477947+Abdullah2O7@users.noreply.github.com>
2025-02-03 14:19:14 +0200

    redeploy

--- main.py
+++ main.py
@@ -125,6 +125,9 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 # def predict():
 #     try:
 #         data = request.get_json()
@@ -144,9 +147,7 @@ def predict():
 #     except Exception as e:
 #         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
 
 
 
